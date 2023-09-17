20f3eb1dca2a3b0212609a813a5b9a77e484b174
Mike Caine <mikey.caine@btinternet.com>
2023-09-17 11:57:33 +0100

    fix some tests

--- gateway/src/test/java/bootiful/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/bootiful/gateway/GatewayApplicationTests.java
@@ -2,9 +2,14 @@ package bootiful.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 
 @SpringBootTest
 class GatewayApplicationTests {
+
+	@MockBean
+	ReactiveClientRegistrationRepository clientRegistrationRepository;
 
 	@Test
 	void contextLoads() {

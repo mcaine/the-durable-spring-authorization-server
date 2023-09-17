c02bece010b2ba8508ed1b273630d40f1246a4ca
Mike Caine <mikey.caine@btinternet.com>
2023-09-17 12:09:26 +0100

    disable ProcessorApplicationTests

--- processor/src/test/java/bootiful/processor/ProcessorApplicationTests.java
+++ processor/src/test/java/bootiful/processor/ProcessorApplicationTests.java
@@ -3,10 +3,10 @@ package bootiful.processor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ProcessorApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
